3f8a448d9281fffef0fcee73b162383b980a46b6
Thiago Souza <thiago@elastic.co>
2018-03-25 14:58:13 -0300

    improve ObjectDispatcher

--- src/main/lib/dispatcher/ObjectDispatcher.ts
+++ src/main/lib/dispatcher/ObjectDispatcher.ts
@@ -8,23 +8,26 @@ export class ObjectDispatcher extends AbstractObjectBuilder {
 
     private filterHelper: ScriptFilterHelper;
 
+    private dispatch: () => void;
+
     constructor(listener: (value?: any) => any, projectExpression: string = '', projectKeys: string[] = []) {
         super();
         this.listener = listener || ((value?: any) => value);
         this.filterHelper = new ScriptFilterHelper(projectKeys, projectExpression);
+
+        this.dispatch = this.filterHelper.isFiltered() ?
+            () => {
+                const result: any = this.peek().value;
+                if (this.filterHelper.filters((key) => key in result)) {
+                    this.listener(result);
+                }
+            } : () => this.listener(this.peek().value);
     }
 
     endObject(): boolean {
         this.doEndObject();
         if (this.isInRoot()) {
-            if (this.filterHelper.isFiltered()) {
-                const result: any = this.peek().value;
-                if (this.filterHelper.filters((key) => key in result)) {
-                    this.listener(result);
-                }
-            } else {
-                this.listener(this.peek().value);
-            }
+            this.dispatch();
             return true;
         }
         return false;
