2b9e3ba32200cf0a4db21a62b2438ce4b3f5c23c
Jason Greenberg <jay.greenberg@elastic.co>
2020-11-01 13:10:53 -0500

    modification to allow typescript consumability

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,15 +2,19 @@ import gulp from 'gulp';
 import gulpCopy from 'gulp-copy';
 import ts from 'gulp-typescript';
 import del from 'del';
+import merge from 'merge2'
 
 const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('clean', () => del('dist'));
 
-gulp.task('compile', () =>
-    tsProject.src().
-        pipe(tsProject()).
-        js.pipe(gulp.dest('dist')));
+gulp.task('compile', () => {
+    let tsResult = tsProject.src().pipe(tsProject());
+    return merge([
+        tsResult.js.pipe(gulp.dest('dist')),
+        tsResult.dts.pipe(gulp.dest('dist'))
+    ]);
+});
 
 
 gulp.task('test-copy-data', () =>
